cf104b46ce3a34167b43c5a3042d9984c83ae40b
DiyaBa <dbakree@gmail.com>
2023-01-17 21:10:22 +0200

    bug11

--- app.js
+++ app.js
@@ -27,10 +27,10 @@ app.set("views", "./views");
 app.use('/',require("./routes/Home"));
 
 // Hosting configrations
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 3000;
 const HOST = "localhost";
 // Start the server
-app.listen(80 , () => {
+app.listen(3000 , () => {
     // console.log(`server is up on ${HOST} : 80`);
 })
 
