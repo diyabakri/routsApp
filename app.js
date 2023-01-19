adc9a8012f460ab95120d1906d848f33782c1f95
DiyaBa <dbakree@gmail.com>
2023-01-19 02:13:34 +0200

    sht

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use('/',require("./routes/Home"));
 // const PORT = process.env.PORT || 4000;
 // const HOST = "localhost";
 // Start the server
-app.listen( process.env.PORT || 4000, () => {
+app.listen( process.env.PORT, () => {
     console.log(`server is up on ${process.env.PORT }`);
 })
 
